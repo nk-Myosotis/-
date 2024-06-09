99b2519adbc29589cc9df5a33274fa483b99a347
yan jiayu <153176236+nk-Myosotis@users.noreply.github.com>
2024-06-10 00:24:51 +0800

    Add files via upload

--- MPI/main.cpp
+++ MPI/main.cpp
@@ -14,27 +14,16 @@ float A[N][N];
 
 void init_A(float arr[][N])
 {
-    for (int i = 0; i < N; i++)
+    for(int i=0;i<N;i++)
     {
-        for (int j = 0; j < N; j++)
-        {
-            arr[i][j] = 0;
-        }
-        arr[i][i] = 1.0;
-        for (int j = i + 1; j < N; j++)
-            arr[i][j] = rand() % 100;
+        arr[i][i]=1.0;
+        for(int j=i+1;j<N;j++)
+            arr[i][j]=rand();
     }
-
-    for (int i = 0; i < N; i++)
-    {
-        int k1 = rand() % N;
-        int k2 = rand() % N;
-        for (int j = 0; j < N; j++)
-        {
-            arr[i][j] += arr[0][j];
-            arr[k1][j] += arr[k2][j];
-        }
-    }
+    for(int k=0;k<N;k++)
+        for(int i=k+1;i<N;i++)
+            for(int j=0;j<N;j++)
+                arr[i][j]+=A[k][j];
 }
 
 
@@ -91,7 +80,7 @@ void ordinary()
 
 
 
-void LU(float A[][N], int rank, int num_proc)
+void block_LU(float A[][N], int rank, int num_proc)
 {
     int block = N / num_proc;
     int remain = N % num_proc;
@@ -179,7 +168,7 @@ void block_mpi()
                     MPI_Send(&A[i * block + j], N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
             }
         }
-        LU(A, rank, num_proc);
+        block_LU(A, rank, num_proc);
         //处理完0号进程自己的任务后需接收其他进程处理之后的结果
         for (int i = 1; i < num_proc; i++)
         {
@@ -219,7 +208,7 @@ void block_mpi()
                 MPI_Recv(&A[rank * block + j], N, MPI_FLOAT, 0, 0,
                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
-        LU(A, rank, num_proc);
+        block_LU(A, rank, num_proc);
         //非0号进程完成任务之后，将结果传回到0号进程
         if (rank != num_proc - 1)
         {
@@ -233,8 +222,183 @@ void block_mpi()
         }
     }
 }
+void block_LU_opt(float A[][N], int rank, int num_proc)
+{
+    __m128 t1, t2, t3;
+    int block = N / num_proc;
+    int remain = N % num_proc;
+    int begin = rank * block;
+    int end = rank != num_proc - 1 ? begin + block : begin + block + remain;
+    #pragma omp parallel for shared(A) private(t1, t2, t3) schedule(dynamic, 24)
+    for (int k = 0; k < N; k++)
+    {
+        if (k >= begin && k < end)
+        {
+            float temp1[4] = { A[k][k], A[k][k], A[k][k], A[k][k] };
+            t1 = _mm_loadu_ps(temp1);
 
+            for (int j = k + 1; j < N - 3; j += 4)
+            {
+                t2 = _mm_loadu_ps(A[k] + j);
+                t3 = _mm_div_ps(t2, t1);
+                _mm_storeu_ps(A[k] + j, t3);
+            }
+            for (int j = N - N % 4; j < N; j++)
+            {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+            #pragma omp parallel for schedule(dynamic, 24)
+            for (int p = rank + 1; p < num_proc; p++)
+                MPI_Send(&A[k], N, MPI_FLOAT, p, 2, MPI_COMM_WORLD);
+        }
+        else
+        {
+            int cur_p = k / block;
+            if (cur_p < rank)
+                MPI_Recv(&A[k], N, MPI_FLOAT, cur_p, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        for (int i = begin; i < end && i < N; i++)
+        {
+            if (i >= k + 1)
+            {
+                float temp2[4] = { A[i][k], A[i][k], A[i][k], A[i][k] };
+                t1 = _mm_loadu_ps(temp2);
+                for (int j = k + 1; j <= N - 3; j += 4)
+                {
+                    t2 = _mm_loadu_ps(A[i] + j);
+                    t3 = _mm_loadu_ps(A[k] + j);
+                    t3 = _mm_mul_ps(t1, t3);
+                    t2 = _mm_sub_ps(t2, t3);
+                    _mm_storeu_ps(A[i] + j, t2);
+                }
+                for (int j = N - N % 4; j < N; j++)
+                    A[i][j] = A[i][j] - A[i][k] * A[k][j];
+                A[i][k] = 0;
+            }
+        }
+    }
+}
 
+void block_mpi_opt()
+{
+
+    timeval t_start;
+    timeval t_end;
+
+    int num_proc;//进程数
+    int rank;//识别调用进程的rank，值从0~size-1
+
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int block = N / num_proc;
+    int remain = N % num_proc;
+
+    //0号进程——任务划分
+    if (rank == 0)
+    {
+        reset_A(A, arr);
+        gettimeofday(&t_start, NULL);
+        //任务划分
+        for (int i = 1; i < num_proc; i++)
+        {
+            if (i != num_proc - 1)
+            {
+                for (int j = 0; j < block; j++)
+                    MPI_Send(&A[i * block + j], N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+            }
+            else
+            {
+                for (int j = 0; j < block + remain; j++)
+                    MPI_Send(&A[i * block + j], N, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+            }
+        }
+        block_LU_opt(A, rank, num_proc);
+        //处理完0号进程自己的任务后需接收其他进程处理之后的结果
+        for (int i = 1; i < num_proc; i++)
+        {
+            if (i != num_proc - 1)
+            {
+                for (int j = 0; j < block; j++)
+                    MPI_Recv(&A[i * block + j], N, MPI_FLOAT, i, 1,
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+            else
+            {
+                for (int j = 0; j < block + remain; j++)
+                    MPI_Recv(&A[i * block + j], N, MPI_FLOAT, i, 1,
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+        }
+        gettimeofday(&t_end, NULL);
+        cout << "Block MPI LU with SSE and OpenMP time cost: "
+            << 1000 * (t_end.tv_sec - t_start.tv_sec) +
+            0.001 * (t_end.tv_usec - t_start.tv_usec) << "ms" << endl;
+    }
+
+    //其他进程
+    else
+    {
+        //非0号进程先接收任务
+        if (rank != num_proc - 1)
+        {
+            for (int j = 0; j < block; j++)
+                MPI_Recv(&A[rank * block + j], N, MPI_FLOAT, 0, 0,
+                    MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        else
+        {
+            for (int j = 0; j < block + remain; j++)
+                MPI_Recv(&A[rank * block + j], N, MPI_FLOAT, 0, 0,
+                    MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        block_LU_opt(A, rank, num_proc);
+        //处理完后向零号进程返回结果
+        if (rank != num_proc - 1)
+        {
+            for (int j = 0; j < block; j++)
+                MPI_Send(&A[rank * block + j], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
+        }
+        else
+        {
+            for (int j = 0; j < block + remain; j++)
+                MPI_Send(&A[rank * block + j], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
+        }
+    }
+}
+void recycle_LU(float A[][N], int rank, int num_proc)
+{
+    for (int k = 0; k < N; k++)
+    {
+        //当前行是自己进程的任务——进行除法
+        if (int(k % num_proc) == rank)
+        {
+            for (int j = k + 1; j < N; j++)
+                A[k][j] = A[k][j] / A[k][k];
+            A[k][k] = 1.0;
+            //除法后向其他进程发送消息
+            for (int p = 0; p < num_proc; p++)
+                if (p != rank)
+                    MPI_Send(&A[k], N, MPI_FLOAT, p, 2, MPI_COMM_WORLD);
+        }
+        //当前行不是自己进程的任务——接收消息
+        else
+        {
+            MPI_Recv(&A[k], N, MPI_FLOAT, int(k % num_proc), 2,
+                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        for (int i = k + 1; i < N; i++)
+        {
+            if (int(i % num_proc) == rank)
+            {
+                for (int j = k + 1; j < N; j++)
+                    A[i][j] = A[i][j] - A[i][k] * A[k][j];
+                A[i][k] = 0.0;
+            }
+        }
+    }
+}
 void recycle_mpi()
 {
     timeval t_start;
@@ -260,7 +424,7 @@ void recycle_mpi()
             else
                 MPI_Send(&A[i], N, MPI_FLOAT, flag, 0, MPI_COMM_WORLD);
         }
-        LU(A, rank, num_proc);
+        recycle_LU(A, rank, num_proc);
         //处理完0号进程自己的任务后需接收其他进程处理之后的结果
         for (int i = 0; i < N; i++)
         {
@@ -285,7 +449,7 @@ void recycle_mpi()
             MPI_Recv(&A[i], N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         //执行任务
-        LU(A, rank, num_proc);
+        recycle_LU(A, rank, num_proc);
         //非0号进程完成任务之后，将结果传回到0号进程
         for (int i = rank; i < N; i += num_proc)
         {
@@ -293,8 +457,61 @@ void recycle_mpi()
         }
     }
 }
+void recycle_LU_opt(float A[][N], int rank, int num_proc)
+{
+    __m128 t1, t2, t3;
+#pragma omp parallel for shared(A) private(t1, t2, t3) schedule(dynamic, 24)
+    for (int k = 0; k < N; k++)
+    {
+        if (int(k % num_proc) == rank)
+        {
+            float temp1[4] = { A[k][k], A[k][k], A[k][k], A[k][k] };
+            t1 = _mm_loadu_ps(temp1);
 
-void pipe_mpi()
+            for (int j = k + 1; j < N - 3; j += 4)
+            {
+                t2 = _mm_loadu_ps(A[k] + j);
+                t3 = _mm_div_ps(t2, t1);
+                _mm_storeu_ps(A[k] + j, t3);
+            }
+            for (int j = N - N % 4; j < N; j++)
+            {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+            #pragma omp parallel for schedule(dynamic, 24)
+            for (int p = 0; p < num_proc; p++)
+                if (p != rank)
+                    MPI_Send(&A[k], N, MPI_FLOAT, p, 2, MPI_COMM_WORLD);
+        }
+        else
+        {
+            MPI_Recv(&A[k], N, MPI_FLOAT, int(k % num_proc), 2,
+                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        for (int i = k + 1; i < N; i++)
+        {
+            if (int(i % num_proc) == rank)
+            {
+                float temp2[4] = { A[i][k], A[i][k], A[i][k], A[i][k] };
+                t1 = _mm_loadu_ps(temp2);
+                for (int j = k + 1; j <= N - 3; j += 4)
+                {
+                    t2 = _mm_loadu_ps(A[i] + j);
+                    t3 = _mm_loadu_ps(A[k] + j);
+                    t3 = _mm_mul_ps(t1, t3);
+                    t2 = _mm_sub_ps(t2, t3);
+                    _mm_storeu_ps(A[i] + j, t2);
+                }
+                for (int j = N - N % 4; j < N; j++)
+                    A[i][j] = A[i][j] - A[i][k] * A[k][j];
+                A[i][k] = 0;
+            }
+        }
+    }
+}
+
+void recycle_mpi_opt()
 {
     timeval t_start;
     timeval t_end;
@@ -309,7 +526,6 @@ void pipe_mpi()
     {
         reset_A(A, arr);
         gettimeofday(&t_start, NULL);
-        //        在0号进程进行任务划分
         for (int i = 0; i < N; i++)
         {
             int flag = i % num_proc;
@@ -318,8 +534,7 @@ void pipe_mpi()
             else
                 MPI_Send(&A[i], N, MPI_FLOAT, flag, 0, MPI_COMM_WORLD);
         }
-        LU(A, rank, num_proc);
-        //处理完0号进程自己的任务后需接收其他进程处理之后的结果
+        recycle_LU_opt(A, rank, num_proc);
         for (int i = 0; i < N; i++)
         {
             int flag = i % num_proc;
@@ -329,19 +544,18 @@ void pipe_mpi()
                 MPI_Recv(&A[i], N, MPI_FLOAT, flag, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         gettimeofday(&t_end, NULL);
-        cout << "Pipeline MPI LU time cost: "
+        cout << "Recycle MPI LU with SSE and OpenMP time cost: "
             << 1000 * (t_end.tv_sec - t_start.tv_sec) +
             0.001 * (t_end.tv_usec - t_start.tv_usec) << "ms" << endl;
+
     }
     else
     {
-        //非0号进程先接收任务
         for (int i = rank; i < N; i += num_proc)
         {
             MPI_Recv(&A[i], N, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
-        LU(A, rank, num_proc);
-        //非0号进程完成任务之后，将结果传回到0号进程
+        recycle_LU_opt(A, rank, num_proc);
         for (int i = rank; i < N; i += num_proc)
         {
             MPI_Send(&A[i], N, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
@@ -349,22 +563,16 @@ void pipe_mpi()
     }
 }
 
-
-
-
-
-
 int main()
 {
     init_A(arr);
-
-    //f_ordinary();
 
     MPI_Init(NULL, NULL);
     ordinary();
     block_mpi();
     recycle_mpi();
-    pipe_mpi();
+    block_mpi_opt();
+    recycle_mpi_opt();
 
     MPI_Finalize();
 
